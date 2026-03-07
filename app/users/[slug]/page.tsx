62813271598740df3f9f902edf2ecb994e7bc694
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-07 13:22:02 +0300

    fix typescript error"

--- app/users/[slug]/page.tsx
+++ app/users/[slug]/page.tsx
@@ -1,12 +1,17 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
+const UserPage = async ({ params }: Props) => {
+  const { slug } = params;
 
-const UserPage = async ( {params} : Promise<{ params: { slug: string }, }>) => {
-    const { slug } = await params;
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-24  ">
-        <h1 className="text-2xl"> User page! : {slug}</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center p-24">
+      <h1 className="text-2xl">User page!: {slug}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
