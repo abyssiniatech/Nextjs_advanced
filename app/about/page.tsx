a4901f3095565808ff31fa660a7b5fb87a2b7f4f
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-07 11:57:30 +0300

    add fetch data ,header and footer section

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4 ">
+        <h1 className="text-2xl">About page!</h1>
+    </div>
+  )
+}
+
+export default page
