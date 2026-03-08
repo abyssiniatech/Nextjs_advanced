e257c9db380086b5f195640ca9c589adf00aff9c
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-08 15:59:14 +0300

    add list

--- app/basic/page.tsx
+++ app/basic/page.tsx
@@ -1,9 +1,10 @@
+import Student from "../components/Student";
+
 export default function Basic(){
-    const name =<h1>Surafel mengist</h1>
+   
     return(
         <div className="min-h-screen flex flex-col justify-center items-center bg-slate-800 text-white">
-            <h1 className="text-3xl font-bold mb-4">Jsx in react next js</h1>
-            <p>My name is {name}</p>
+            <Student />
         </div>
     )
 }
