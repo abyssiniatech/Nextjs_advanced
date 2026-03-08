91dbe3be248479c6fac8cdddcebddb1062208d33
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-08 16:29:02 +0300

    add counter app and its logic"

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Counter from "../components/projects/Counter"
+
+const page = () => {
+  return (
+    <div className="bg-gray-100 text-gray-800 min-h-screen flex items-center justify-center p-4"> 
+      <Counter />
+    </div>
+  )
+}
+
+export default page
