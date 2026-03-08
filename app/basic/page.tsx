2aa1ea55130c80fae7fd08d14ce4d4a6f75420fa
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-08 15:48:40 +0300

    add intial commit for day4

---
+++ app/basic/page.tsx
@@ -0,0 +1,9 @@
+export default function Basic(){
+    const name =<h1>Surafel mengist</h1>
+    return(
+        <div className="min-h-screen flex flex-col justify-center items-center bg-slate-800 text-white">
+            <h1 className="text-3xl font-bold mb-4">Jsx in react next js</h1>
+            <p>My name is {name}</p>
+        </div>
+    )
+}
