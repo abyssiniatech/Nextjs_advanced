576bb0421b5a238bd5225ab31249da3d8f343854
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-14 02:29:16 +0300

    add logic

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
-export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: number }> }) {
       const { slug } = await params;
     return (
         <div className="bg-indigo-900 text-white flex justify-center items-center min-h-screen">
-            <h1> the student is is : {slug}</h1>
+            <h1> the student is is : <span style={{color:slug<50 ? "red" : slug>50? "teal":"green" }}>{slug}</span></h1>
         </div>
     );
 }
